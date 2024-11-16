7f1961c93e336cbda15f42272e186e353c3bd3bf
z1mer <101604219+z1mer-off@users.noreply.github.com>
2024-11-17 01:36:29 +0300

    LR 1.3 - Избавился от повторяющегося кода в конструкторе и аксессорах благодаря преобразованию класса в data class

--- Main.kt
+++ Main.kt
@@ -1,9 +1,9 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("Романов","Владислав","Витальевич"));
-    students.add(Student("Овдиенко","Артём","Сергеевич", studentTelegram = "@aaaaaaa"));
-    students.add(Student("Придава","Александр","Александрович", studentGit = "megaladon"));
-    students.add(Student("Полевая","Полина","Андреевна", studentEmail = "shroudolka@gmail.com"));
+    students.add(Student("Овдиенко","Артём","Сергеевич", telegram = "@aaaaaaa"));
+    students.add(Student("Придава","Александр","Александрович", gitHub = "megaladon"));
+    students.add(Student("Полевая","Полина","Андреевна", email = "shroudolka@gmail.com"));
     students.add(Student("Абрамов","Иван","Дмитриевич"));
     students.add(Student("Гонтарев","Александр","Дмитриевич"));
     students.add(Student("Попов","Иван","Викторович"));
--- Student.kt
+++ Student.kt
@@ -1,4 +1,14 @@
-class Student{
+data class Student(
+    var surname:String,
+    var name:String,
+    var patronymic:String,
+    var id:Int?= autoGenerateId(),
+    var phoneNumber:String?=null,
+    var telegram:String?=null,
+    var email:String?=null,
+    var gitHub:String?=null
+)
+{
     companion object{
         var classId:Int = 0;
         fun autoGenerateId():Int{
@@ -6,83 +16,3 @@ class Student{
             return classId;
         }
     }
-    var id:Int?
-        get():Int?{
-            return field;
-        }
-        set(value:Int?) {
-            field = value;
-        };
-    var surname:String
-        get():String{
-            return field;
-        }
-        set(value:String) {
-            field = value;
-        };
-    var name:String
-        get():String{
-            return field;
-        }
-        set(value:String) {
-            field = value;
-        };
-    var patronymic:String
-        get():String{
-            return field;
-        }
-        set(value:String) {
-            field = value;
-        };
-    var phoneNumber:String?
-        get():String?{
-            return field;
-        }
-        set(value:String?) {
-            field = value;
-        };
-    var telegram:String?
-        get():String?{
-            return field;
-        }
-        set(value:String?) {
-            field = value;
-        };
-    var email:String?
-        get():String?{
-            return field;
-        }
-        set(value:String?) {
-            field = value;
-        };
-    var gitHub:String?
-        get():String?{
-            return field;
-        }
-        set(value:String?) {
-            field = value;
-        };
-    constructor(
-        studentSurname:String,
-        studentName:String,
-        studentPatronymic:String,
-        studentId:Int?= autoGenerateId(),
-        studentPhone:String?=null,
-        studentTelegram:String?=null,
-        studentEmail:String?=null,
-        studentGit:String?=null
-    )
-    {
-        this.id = studentId;
-        this.surname = studentSurname;
-        this.name = studentName;
-        this.patronymic = studentPatronymic;
-        this.phoneNumber = studentPhone;
-        this.telegram = studentTelegram;
-        this.email = studentEmail;
-        this.gitHub = studentGit;
-    }
-    override fun toString(): String {
-        return "ID:${this.id} Surname:${this.surname} Name:${this.name} Patronymic:${this.patronymic} Phone:${this.phoneNumber} Telegram:${this.telegram} Email:${this.email} Git:${this.gitHub}";
-    }
-}
