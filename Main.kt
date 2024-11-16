718099027c8e584b0649486bc256bf3c0f29b1f7
z1mer <101604219+z1mer-off@users.noreply.github.com>
2024-11-17 00:35:45 +0300

    LR 1.2 - для корректного вывода экземпляра класса добавил метод toString

--- Main.kt
+++ Main.kt
@@ -1,6 +1,11 @@
 fun main() {
-    val student = Student("Овдиенко","Артём","Сергеевич");
-    println(student.name);
-    student.name = "Вячеслав";
-    println(student.name);
+    val students = mutableListOf<Student>();
+    students.add(Student("Романов","Владислав","Витальевич"));
+    students.add(Student("Овдиенко","Артём","Сергеевич", studentTelegram = "@aaaaaaa"));
+    students.add(Student("Придава","Александр","Александрович", studentGit = "megaladon"));
+    students.add(Student("Полевая","Полина","Андреевна", studentEmail = "shroudolka@gmail.com"));
+    students.add(Student("Абрамов","Иван","Дмитриевич"));
+    students.add(Student("Гонтарев","Александр","Дмитриевич"));
+    students.add(Student("Попов","Иван","Викторович"));
+    students.forEach { it: Student -> println(it) };
 }
--- Student.kt
+++ Student.kt
@@ -6,7 +6,6 @@ class Student{
             return classId;
         }
     }
-
     var id:Int?
         get():Int?{
             return field;
@@ -67,13 +66,14 @@ class Student{
         studentSurname:String,
         studentName:String,
         studentPatronymic:String,
+        studentId:Int?= autoGenerateId(),
         studentPhone:String?=null,
         studentTelegram:String?=null,
         studentEmail:String?=null,
         studentGit:String?=null
     )
     {
-        this.id = autoGenerateId();
+        this.id = studentId;
         this.surname = studentSurname;
         this.name = studentName;
         this.patronymic = studentPatronymic;
@@ -81,5 +81,8 @@ class Student{
         this.telegram = studentTelegram;
         this.email = studentEmail;
         this.gitHub = studentGit;
+    }
+    override fun toString(): String {
+        return "ID:${this.id} Surname:${this.surname} Name:${this.name} Patronymic:${this.patronymic} Phone:${this.phoneNumber} Telegram:${this.telegram} Email:${this.email} Git:${this.gitHub}";
     }
 }
