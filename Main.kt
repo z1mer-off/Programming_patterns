31613fe225828a9762639807ca998eb4ac8e0035
z1mer <101604219+z1mer-off@users.noreply.github.com>
2024-11-17 04:33:20 +0300

    LR 1.7 - Создана валидация атрибутов экземпляра класса Student

--- Main.kt
+++ Main.kt
@@ -1,12 +1,10 @@
 fun main() {
     val students = mutableListOf<Student>();
-    students.add(Student("Романов","Владислав","Витальевич"));
-    students.add(Student("Овдиенко","Артём","Сергеевич", studentTelegram = "@aaaaaaaa"));
-    students.add(Student("Придава","Александр","Александрович", gitHub = "megaladon"));
-    students.add(Student("Полевая","Полина","Андреевна", email = "shroudolka@gmail.com"));
-    students.add(Student("Абрамов","Иван","Дмитриевич"));
-    students.add(Student("Гонтарев","Александр","Дмитриевич"));
-    students.add(Student("Попов","Иван","Викторович"));
-    students.add(Student(hashMapOf(Pair("name", "Артём"), Pair("surname", "Овдиенко"), Pair("patronymic", "Сергеевич"))));
+    students.add(Student("Овдиенко","Артём","Сергеевич"));
+    students.add(Student("Мамин-Сибиряк","Дмитрий","Наркисович", telegram = "@poet123"));
+    students.add(Student("Махмудов","Арарат-Магомед","Саркисович", gitHub = "abu0987"));
+    students.add(Student("Лермонтов","Михаил","Юрьевич", email = "literatureclub@gmail.com"));
+    students.add(Student("Городецкий","Эдуард","Романович", phoneNumber = "+78005553535"));
+    students.add(Student(hashMapOf(Pair("name","Владислав"),Pair("surname","Романов"),Pair("patronymic","Витальевич"))));
     students.forEach { it: Student -> println(it) };
 }
--- Student.kt
+++ Student.kt
@@ -11,8 +11,32 @@ data class Student(
 {
     init {
         require(
+            Student.isValidSurname(this.surname),
+            { "Фамилия должна быть действительной" }
+        );
+        require(
+            Student.isValidName(this.name),
+            { "Имя должно быть действительным" }
+        );
+        require(
+            Student.isValidPatronymic(this.patronymic),
+            { "Отчество должно быть действительным" }
+        );
+        require(
             Student.isValidPhone(this.phoneNumber),
             { "Номер телефона должен быть действительным" }
+        );
+        require(
+            Student.isValidTelegram(this.telegram),
+            { "Телеграм должен быть действительным" }
+        );
+        require(
+            Student.isValidEmail(this.email),
+            { "Адрес электронной почты должен быть действительным" }
+        );
+        require(
+            Student.isValidGitHub(this.gitHub),
+            { "Git должен быть действительным" }
         );
     }
     companion object{
@@ -24,13 +48,37 @@ data class Student(
         fun isValidPhone(phone: String?): Boolean {
             return phone?.matches(Regex("\\+7\\d{10}")) ?: true;
         }
+
+        fun isValidSurname(surname: String): Boolean {
+            return surname.matches(Regex("^[А-Я][а-я]*(-([А-Яа-я]?)[а-я]*)*$"));
+        }
+
+        fun isValidName(name: String): Boolean {
+            return name.matches(Regex("^[А-Я][а-я]*(-([А-Яа-я]?)[а-я]*)*$"));
+        }
+
+        fun isValidPatronymic(patronymic: String): Boolean {
+            return patronymic.matches(Regex("^[А-Я][а-я]*(-([А-Яа-я]?)[а-я]*)*$"));
+        }
+
+        fun isValidTelegram(telegram: String?): Boolean {
+            return telegram?.matches(Regex("@(?=.{5,64})(?!_)(?!.*__)[a-zA-Z0-9_]+(?<![_.])")) ?: true;
+        }
+
+        fun isValidEmail(email: String?): Boolean {
+            return email?.matches(Regex("^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$")) ?: true;
+        }
+
+        fun isValidGitHub(gitHub: String?): Boolean {
+            return gitHub?.let { !Regex("[$%#@&/?]").matches(it) } ?: true;
+        }
     }
     constructor(studentArgs: HashMap<String,Any?>) : this(id = studentArgs.getOrDefault("id", autoGenerateId()) as Int,
         surname = studentArgs["surname"].toString(),
         name = studentArgs["name"].toString(),
         patronymic = studentArgs["patronymic"].toString(),
         phoneNumber = studentArgs.getOrDefault("phoneNumber",null) as String?,
-        telegram = studentArgs.getOrDefault("telegram",null).toString(),
-        email = studentArgs.getOrDefault("email",null).toString(),
-        gitHub = studentArgs.getOrDefault("gitHub",null).toString()){}
+        telegram = studentArgs.getOrDefault("telegram",null) as String?,
+        email = studentArgs.getOrDefault("email",null) as String?,
+        gitHub = studentArgs.getOrDefault("gitHub",null) as String?) {}
 }
