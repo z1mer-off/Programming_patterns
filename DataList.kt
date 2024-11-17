38ee7c0b8000ca630b897540eba5d1d80c9241ae
z1mer <101604219+z1mer-off@users.noreply.github.com>
2024-11-18 02:58:39 +0300

    LR 2.17 - Изменены методы get_data и get_names

--- DataList.kt
+++ DataList.kt
@@ -1,4 +1,6 @@
-abstract class DataList<T>(val elements: Array<T>) {
+import kotlin.reflect.full.memberProperties
+
+open class DataList<T>(val elements: Array<T>) {
     private val selectedIndices = mutableSetOf<Int>()
     // Выделение элемента по номеру
     fun select(number: Int) {
@@ -8,10 +10,25 @@ abstract class DataList<T>(val elements: Array<T>) {
             throw IndexOutOfBoundsException("Индекс вне диапазона")
         }
     }
+
     // Получение массива ID выделенных элементов
     fun getSelectedIds() = selectedIndices.toIntArray()
+
     // Получение массива наименований атрибутов
-    abstract fun getNames(): Array<String>
+    open fun functionGetPropsNames() = if(this.elements.isEmpty()) arrayOf() else this.elements[0]!!::class.memberProperties.map{it.name}.toTypedArray<String>()
+    fun getNames(): Array<String> = functionGetPropsNames()
+
     // Получение DataTable
-    abstract fun getData(): DataTable
+    open fun getPropertiesOfClass(value:T):List<Any?> = arrayOf("12",2).toList()
+    fun getData(): DataTable {
+        val dataList:MutableList<Array<Any?>> = mutableListOf()
+        var rowNumber = 0;
+        for(el in this.elements){
+            dataList.add(
+                arrayOf(rowNumber,*getPropertiesOfClass(el).toTypedArray<Any?>())
+            )
+            rowNumber+=1;
+        }
+        return DataTable(dataList.toTypedArray<Array<Any?>>())
+    }
 }
--- DataListStudentShort.kt
+++ DataListStudentShort.kt
@@ -1,14 +1,4 @@
 class DataListStudentShort(studentShortArray: Array<StudentShort>):DataList<StudentShort>(studentShortArray) {
-    override fun getNames(): Array<String> = StudentShort.returnPropertyNames().toTypedArray<String>()
-    override fun getData(): DataTable {
-        val dataList:MutableList<Array<Any?>> = mutableListOf()
-        var rowNumber = 0;
-        for(student in this.elements){
-            dataList.add(
-                arrayOf(rowNumber,*(student as StudentShort).propertiesReturn().values.drop(1).toTypedArray())
-            )
-            rowNumber+=1;
-        }
-        return DataTable(dataList.toTypedArray<Array<Any?>>())
-    }
+    override fun functionGetPropsNames(): Array<String> = StudentShort.returnPropertyNames().toTypedArray<String>()
+    override fun getPropertiesOfClass(value: StudentShort): List<Any?> = value.propertiesReturn().values.drop(1).toList()
 }
